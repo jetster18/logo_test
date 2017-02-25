d3815eb5144237e4c509f2223f5cd808933c25c8
Jetster18 <mccord.70@osu.edu>
2017-02-25 16:13:13 -0500

    First draft

--- Logo_Test/app/src/main/java/com/example/itintern3/logo_test/MainActivity.java
+++ Logo_Test/app/src/main/java/com/example/itintern3/logo_test/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.itintern3.logo_test;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.Console;
+
+import static com.example.itintern3.logo_test.R.id.activity_main;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,23 +19,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView img= (ImageView) findViewById(R.id.image);
-        img.setImageResource(R.drawable.logo);
 
 
 
 
-        // next button on click
-        final Button nextButton = (Button) findViewById(R.id.next_button);
-        nextButton.setOnClickListener(new View.OnClickListener() {
+        final ImageView img= (ImageView) findViewById(R.id.image_switcher);
+        //img.setImageResource(R.drawable.logo);
+        //img.setImageDrawable(ContextCompat(activity_main));
+        //img.setImageResource(R.drawable.logo);
+
+
+
+
+
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
+                //Log.d("next", "next button pressed");
                 img.setImageResource(R.drawable.twitter);
             }
         });
 
-        final Button backButton = (Button) findViewById(R.id.back_button);
-        backButton.setOnClickListener(new View.OnClickListener() {
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
                 img.setImageResource(R.drawable.facebook);
@@ -37,5 +50,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        final Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                img.setImageResource(R.drawable.logo);
+            }
+        });
+
+
+
+
+
+
     }
+
 }
